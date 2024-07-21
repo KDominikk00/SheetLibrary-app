1baac723fab928c6572f13954668cd4197ceb937
KDominikk00 <dominikkowalski331@gmail.com>
2024-07-21 20:07:43 +0000

    Add routes and temporary REST API connection

--- SheetLibrary/src/App.jsx
+++ SheetLibrary/src/App.jsx
@@ -1,11 +1,34 @@
 import './App.css'
-import Header from "./components/Header"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Explore from "./pages/Explore"
+import NoPage from './pages/NoPage'
+import { useEffect, useState } from 'react'
 
 function App() {
+
+  const [title, setTitle] = useState(0)
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch("http://localhost:8080/sheet")
+      result.json().then(json => {
+        console.log(json)
+      })
+    }
+    fetchData()
+  })
+
   return (
     <div className="App">
-      <Header />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/explore" element={<Explore />} />
+        <Route path="*" element={<NoPage />} />
+      </Routes>
+    </BrowserRouter>
+    </div>  
   )
 }
 
